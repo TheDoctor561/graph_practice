ca48e1ce5810fb007c10d24b4f28db4b2f857c4d
Dan <daniel.yucua@gmail.com>
2022-07-24 16:07:42 -0400

    wowzers

--- depthFirst.py
+++ depthFirst.py
@@ -7,5 +7,22 @@ graph = {
   'd': ['f'], 
   'e': [], 
   'f': []
-  }
+  } 
 
+# a --> c
+# |     |
+# \/   \/ 
+# b     e 
+# | 
+# \/
+# d --> f 
+
+def depthFirstPrint(graph, source): 
+  stack = [source]
+  while len(stack) > 0: 
+    curr = stack.pop(0) 
+    print(curr)
+    for i in graph[curr]: 
+      stack.append(i)
+
+depthFirstPrint(graph, 'a')
