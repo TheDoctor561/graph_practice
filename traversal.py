53bb33ccdf37d82afe66ab63e2c906f90fa126c1
Dan <daniel.yucua@gmail.com>
2022-07-24 17:01:07 -0400

    changed name

--- traversal.py
+++ traversal.py
@@ -20,9 +20,22 @@ graph = {
 def depthFirstPrint(graph, source): 
   stack = [source]
   while len(stack) > 0: 
+
+    # Utilization of a queue
+    curr = stack.pop() 
+    print(curr)
+    for i in graph[curr]: 
+      stack.append(i)
+
+def breadthFirstPrint(graph, source): 
+  stack = [source]
+  while len(stack) > 0: 
+    # Utilization of a stack
     curr = stack.pop(0) 
     print(curr)
     for i in graph[curr]: 
       stack.append(i)
 
 depthFirstPrint(graph, 'a')
+
+breadthFirstPrint(graph, 'a')
